a9d93b171714cfd30565940927def69070d0b76c
MadhavChoudhary <madhavtummala@icloud.com>
2018-12-10 00:40:35 +0530

    New simple shell script

---
+++ scan.sh
@@ -0,0 +1,3 @@
+IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+IP=$(echo $IP | cut -d"." -f1-3)
+fping -a -t10 -r0 -q -g $IP".1/24"
