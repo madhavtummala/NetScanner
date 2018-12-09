8fe58e01dcea7efdb1c775849e71d08579e3d753
MadhavChoudhary <madhavtummala@icloud.com>
2018-12-09 23:38:14 +0530

    GUI based on tqdm

--- scanner.py
+++ scanner.py
@@ -4,7 +4,7 @@ import socket
 BASE_IP = socket.gethostbyname(socket.gethostname())
 BASE_IP = BASE_IP[:BASE_IP.rfind(".")] + "."
 
-for i in range(0,256):
+for i in tqdm.tqdm(range(256)):
 	IP = BASE_IP+str(i)
 	response = os.system("fping -c1 -t10 " + IP + " > /dev/null 2>&1")
 	if response == 0:
