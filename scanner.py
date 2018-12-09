a5ce0145d710965f00f7b098c10f576a7c735225
MadhavChoudhary <madhavtummala@icloud.com>
2018-12-09 23:44:29 +0530

    Made Final GUI

--- scanner.py
+++ scanner.py
@@ -1,18 +1,17 @@
 import os
 import socket
-import progressbar
+import tqdm
 
 BASE_IP = socket.gethostbyname(socket.gethostname())
 BASE_IP = BASE_IP[:BASE_IP.rfind(".")] + "."
 
-bar = progressbar.ProgressBar(maxval=256, \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+my_list = []
 
-bar.start()
-for i in range(256):
+for i in tqdm.tqdm(range(256)):
 	IP = BASE_IP+str(i)
-	bar.update(i)
 	response = os.system("fping -c1 -t10 " + IP + " > /dev/null 2>&1")
 	if response == 0:
-		print (IP)
-bar.finish()
+		my_list.append(IP)
+
+for i in my_list:
+	print(i)
